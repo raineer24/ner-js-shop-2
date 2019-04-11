fd4237f36062f5b4adb91882f4e236cc08b08d66
raineer <delaritaraineer81@gmail.com>
2019-04-11 15:39:42 +0800

    Show data array

--- app.js
+++ app.js
@@ -9,3 +9,37 @@ const cartItems = document.querySelector('.cart-items');
 const cartTotal = document.querySelector('.cart-total');
 const cartContent = document.querySelector('.cart-content');
 const productsDOM = document.querySelector('.products-center');
+
+
+//cart
+let cart = [];
+
+
+//getting the products 
+class Products {
+    async getProducts() {
+        try {
+            let result = await fetch('products.json');
+            let data = await result.json();
+            return data;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+class UI {
+
+}
+
+class Storage {
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const ui = new UI();
+    const products = new Products();
+   
+    //get all products
+    products.getProducts().then(data => console.log(data));
+
+});
